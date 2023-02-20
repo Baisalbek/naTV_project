b32f531bd409fcc8a09f7c215f26c34f9026e508
Baisal <bais0638@gmail.com>
2023-02-20 16:40:26 +0300

    test commit

---
+++ src/test/java/kg/mega/naTV/NaTvApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.mega.naTV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaTvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
